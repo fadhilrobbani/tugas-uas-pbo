9bcbcf82f1129350a0bcb509ef8a89743b755552
fadhilrobbani <rabbanifadhillailham@gmail.com>
2023-05-31 21:59:32 +0700

    change

--- hangman.py
+++ hangman.py
@@ -1,139 +1,145 @@
 import random
 
-def hangman():
-    word_list = ["python", "hangman", "computer", "programming", "game", "openai"]
-    chosen_word = random.choice(word_list)
-    guessed_letters = []
-    tries = 6
+class Hangman:
+    def __init__(self):
+        self.word_list = ["python", "hangman", "computer", "programming", "game", "openai"]
+        self.chosen_word = random.choice(self.word_list)
+        self.guessed_letters = []
+        self.tries = 6
 
-    hangman_states = [
-        """
-           _______
-          |/      |
-          |      
-          |      
-          |       
-          |      
-          |
-         _|_
-        |   |______
-        |          |
-        |__________|
-        """,
-        """
-           _______
-          |/      |
-          |      (_)
-          |      
-          |       
-          |      
-          |
-         _|_
-        |   |______
-        |          |
-        |__________|
-        """,
-        """
-           _______
-          |/      |
-          |      (_)
-          |       |
-          |       |
-          |       
-          |
-         _|_
-        |   |______
-        |          |
-        |__________|
-        """,
-        """
-           _______
-          |/      |
-          |      (_)
-          |      \\|
-          |       |
-          |       
-          |
-         _|_
-        |   |______
-        |          |
-        |__________|
-        """,
-        """
-           _______
-          |/      |
-          |      (_)
-          |      \\|/
-          |       |
-          |       
-          |
-         _|_
-        |   |______
-        |          |
-        |__________|
-        """,
-        """
-           _______
-          |/      |
-          |      (_)
-          |      \\|/
-          |       |
-          |      / 
-          |
-         _|_
-        |   |______
-        |          |
-        |__________|
-        """,
-        """
-           _______
-          |/      |
-          |      (_)
-          |      \\|/
-          |       |
-          |      / \\
-          |
-         _|_
-        |   |______
-        |          |
-        |__________|
-        """
-    ]
+    def play(self):
+        while self.tries > 0:
+            word_status = ""
+            for letter in self.chosen_word:
+                if letter in self.guessed_letters:
+                    word_status += letter
+                else:
+                    word_status += "_"
 
-    while tries > 0:
-        word_status = ""
-        for letter in chosen_word:
-            if letter in guessed_letters:
-                word_status += letter
-            else:
-                word_status += "_"
+            if word_status == self.chosen_word:
+                print("Congratulations! You guessed the word:", self.chosen_word)
+                break
 
-        if word_status == chosen_word:
-            print("Congratulations! You guessed the word:", chosen_word)
-            break
+            self.display_hangman()
+            print("Guess the word:", word_status)
+            print("Tries left:", self.tries)
 
-        print(hangman_states[6 - tries])
-        print("Guess the word:", word_status)
-        print("Tries left:", tries)
+            guess = input("Enter a letter: ").lower()
 
-        guess = input("Enter a letter: ").lower()
+            if len(guess) != 1:
+                print("Please enter a single letter.")
+                continue
 
-        if len(guess) != 1:
-            print("Please enter a single letter.")
-            continue
+            if guess in self.guessed_letters:
+                print("You've already guessed that letter.")
+                continue
 
-        if guess in guessed_letters:
-            print("You've already guessed that letter.")
-            continue
+            if guess not in self.chosen_word:
+                print("Wrong guess!")
+                self.tries -= 1
 
-        if guess not in chosen_word:
-            print("Wrong guess!")
-            tries -= 1
+            self.guessed_letters.append(guess)
 
-        guessed_letters.append(guess)
+        else:
+            self.display_hangman()
+            print("Game over! The word was", self.chosen_word)
 
-    else:
-        print(hangman_states[6 - tries])
-        print("Game over! The word was", chosen_word)
+    def display_hangman(self):
+        hangman_states = [
+            """
+               _______
+              |/      |
+              |      
+              |      
+              |       
+              |      
+              |
+             _|_
+            |   |______
+            |          |
+            |__________|
+            """,
+            """
+               _______
+              |/      |
+              |      (_)
+              |      
+              |       
+              |      
+              |
+             _|_
+            |   |______
+            |          |
+            |__________|
+            """,
+            """
+               _______
+              |/      |
+              |      (_)
+              |       |
+              |       |
+              |       
+              |
+             _|_
+            |   |______
+            |          |
+            |__________|
+            """,
+            """
+               _______
+              |/      |
+              |      (_)
+              |      \\|
+              |       |
+              |       
+              |
+             _|_
+            |   |______
+            |          |
+            |__________|
+            """,
+            """
+               _______
+              |/      |
+              |      (_)
+              |      \\|/
+              |       |
+              |       
+              |
+             _|_
+            |   |______
+            |          |
+            |__________|
+            """,
+            """
+               _______
+              |/      |
+              |      (_)
+              |      \\|/
+              |       |
+              |      / 
+              |
+             _|_
+            |   |______
+            |          |
+            |__________|
+            """,
+            """
+               _______
+              |/      |
+              |      (_)
+              |      \\|/
+              |       |
+              |      / \\
+              |
+             _|_
+            |   |______
+            |          |
+            |__________|
+            """
+        ]
 
-hangman()
+        print(hangman_states[6 - self.tries])
+
+game = Hangman()
+game.play()
